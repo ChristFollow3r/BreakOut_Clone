36cf66151d48870ec4ef75b834ff4a582086afa4
Nil <nilbadia@gmail.com>
2026-02-28 20:37:15 +0100

    Fixed thing size decreasing thing

--- Assets/Scripts/ballMovement.cs
+++ Assets/Scripts/ballMovement.cs
@@ -5,7 +5,7 @@ public class ballMovement : MonoBehaviour
     [SerializeField] private GameObject thing;
     [SerializeField] private Rigidbody2D ball;
 
-    private float ballSpeed = 1.2f;
+    private float ballSpeed = 1.3f;
     private float sizeDecrese = 0.2f;
     private Vector3 newScale;
 
@@ -19,6 +19,7 @@ public class ballMovement : MonoBehaviour
         float num1 = Random.Range(3, 5);
         float num2 = Random.Range(3, 5);
         newScale = thing.transform.localScale;
+        newScale -= new Vector3(sizeDecrese,0,0);
 
         ball.linearVelocity = new Vector2(num1, num2);
     }
@@ -39,34 +40,35 @@ public class ballMovement : MonoBehaviour
         if (collision.gameObject.layer == 7 && layer2)
         {
             ball.linearVelocity *= ballSpeed;
-            thing.GetComponent<thingMovement>().thingSpeed += 1.5f;
-            newScale.x -= sizeDecrese;
-            thing.transform.localScale += newScale;
+            thing.GetComponent<thingMovement>().thingSpeed += 1.2f;
+            thing.transform.localScale = newScale;
+            newScale -= new Vector3(sizeDecrese, 0, 0);
             layer2 = false;
         }
 
         else if (collision.gameObject.layer == 8 && layer3)
         {
             ball.linearVelocity *= ballSpeed;
-            thing.GetComponent<thingMovement>().thingSpeed += 1.5f;
-            newScale.x -= sizeDecrese;
-            thing.transform.localScale += newScale;
+            thing.GetComponent<thingMovement>().thingSpeed += 1.2f;
+            thing.transform.localScale = newScale;
+            newScale -= new Vector3(sizeDecrese, 0, 0);
             layer3 = false;
         }
         else if (collision.gameObject.layer == 9 && layer4)
         {
             ball.linearVelocity *= ballSpeed;
-            thing.GetComponent<thingMovement>().thingSpeed += 1.5f;
+            thing.GetComponent<thingMovement>().thingSpeed += 1.2f;
             newScale.x -= sizeDecrese;
-            thing.transform.localScale += newScale;
+            thing.transform.localScale = newScale;
+            newScale -= new Vector3(sizeDecrese, 0, 0);
             layer4 = false;
         }
         else if (collision.gameObject.layer == 10 && layer5)
         {
             ball.linearVelocity *= ballSpeed;
-            thing.GetComponent<thingMovement>().thingSpeed += 1.5f;
-            newScale.x -= sizeDecrese;
-            thing.transform.localScale += newScale;
+            thing.GetComponent<thingMovement>().thingSpeed += 1.2f;
+            thing.transform.localScale = newScale;
+            newScale -= new Vector3(sizeDecrese, 0, 0);
             layer5 = false;
         }
     }
