874de88d056de559d5eb3f1a9072bdb90e5d4b60
Nil <nilbadia@gmail.com>
2026-03-01 09:40:44 +0100

    Added upwards movement on ball hitting the middle of the thing

--- Assets/Scripts/ballMovement.cs
+++ Assets/Scripts/ballMovement.cs
@@ -26,10 +26,12 @@ public class ballMovement : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Right")) ball.linearVelocity *= new Vector2 (-1, 1);
-        else if (collision.CompareTag("Left")) ball.linearVelocity *= new Vector2(-1, 1);
+        float currentBallSpeed = ball.linearVelocity.magnitude;
+        if (collision.CompareTag("Right")) ball.linearVelocity *= new Vector2 (-1, 1); // I can see this is repeated code but I tried to change it and it gave me errors
+        else if (collision.CompareTag("Left")) ball.linearVelocity *= new Vector2(-1, 1); // So it'll stay like this...
         else if (collision.CompareTag("Top")) ball.linearVelocity *= new Vector2(1, -1);
         else if (collision.CompareTag("Thing")) ball.linearVelocity *= new Vector2(1, -1);
+        else if (collision.CompareTag("Middle")) ball.linearVelocity = new Vector2(0, currentBallSpeed); // To do: Decrease raycast length and collider size
 
         if (collision.gameObject.GetComponent<brickLife>())
         {
