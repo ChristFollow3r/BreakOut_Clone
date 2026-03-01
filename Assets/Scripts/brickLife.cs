4be6a6b38277ee5b58bee9167b93693514cdc3e3
Nil <nilbadia@gmail.com>
2026-03-01 16:41:07 +0100

    Added two missing upgrade prefabs

--- Assets/Scripts/brickLife.cs
+++ Assets/Scripts/brickLife.cs
@@ -5,7 +5,9 @@ public class brickLife : MonoBehaviour
     [SerializeField] public int lives;
     [SerializeField] private GameObject UIManager; // This works on all the prefabs cause I manually added it on all the prefabs :V
     // I could just do GameObject.Find() but it already works so I'm not touching it.
-    [SerializeField] private GameObject upgrade;
+    [SerializeField] private GameObject sBallUpgrade;
+    [SerializeField] private GameObject plentyBallsUpgrade;
+    [SerializeField] private GameObject shotgunUpgrade;
 
     private SpriteRenderer sprite;
     private BoxCollider2D colldier;
@@ -20,7 +22,12 @@ public class brickLife : MonoBehaviour
     {
         if (lives <= 0)
         {
-            Instantiate(upgrade, transform.position, Quaternion.identity);
+            int chance = Random.Range(1, 101);
+
+            if (chance <= 10) Instantiate(sBallUpgrade, transform.position, Quaternion.identity);
+            else if (chance > 10 && chance <= 20) Instantiate(shotgunUpgrade, transform.position, Quaternion.identity);
+            else if (chance > 20 && chance <= 30) Instantiate(plentyBallsUpgrade, transform.position, Quaternion.identity);
+
             Destroy(sprite);
             Destroy(colldier);
             Destroy(gameObject);
